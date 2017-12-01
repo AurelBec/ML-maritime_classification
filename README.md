f735b213649b2f16baf1fa326ec11a4d6f3eed6b
Aurelien BEC <aurelien.bec@gmail.com>
2017-12-01 01:07:01 +0100

    publish

---
+++ README.md
@@ -0,0 +1,10 @@
+# Maritime Classifier
+
+really easy CNN<b>
+
+Use tensorflow and kera librairies<b><b>
+
+data_sets need to contain folders named with the name of the class the pictures inside belong<b>
+*ex : TRAINING_SET/*<b>
+*        cats/*<b>
+*           imgs*
---
+++ cnn_maritime_classifier.py
@@ -0,0 +1,41 @@
+# Convolutional Neural Network
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+# 1 - Building the CNN
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+classifier.add(Conv2D(64, 3, 3, input_shape = (100, 30, 3), activation = 'relu')) # Convolution layer
+classifier.add(MaxPooling2D(pool_size = (2, 2))) # Pooling layer
+classifier.add(Conv2D(64, 3, 3, activation = 'relu')) # Adding a second convolutional layer
+classifier.add(MaxPooling2D(pool_size = (2, 2))) # Adding a second convolutional layer
+classifier.add(Flatten()) # Flattening layer
+classifier.add(Dense(units = 128, activation = 'relu')) # Full connection
+classifier.add(Dense(units = 24, activation = 'sigmoid')) # Full connection
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+
+# 2 - Fitting the CNN to the images
+from keras.preprocessing.image import ImageDataGenerator
+
+training_set = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True).flow_from_directory('dataset/training_set', target_size = (100, 30))
+test_set = ImageDataGenerator(rescale = 1./255).flow_from_directory('dataset/test_set', target_size = (100, 30))
+classifier.fit_generator(training_set, steps_per_epoch = 4774, epochs = 10, validation_data = test_set, validation_steps = 1682)
+
+
+# 3 - Making new predictions
+import numpy as np
+from keras.preprocessing import image
+
+file = 'dataset/test_set/Polizia/20130412_064127_20371.jpg'
+result = classifier.predict(np.expand_dims(image.img_to_array(image.load_img(file, target_size = (100, 30))), axis = 0))
+id = test_set.class_indices
+print(list(id.keys())[list(id.values()).index(np.argmax(result[0]))])
